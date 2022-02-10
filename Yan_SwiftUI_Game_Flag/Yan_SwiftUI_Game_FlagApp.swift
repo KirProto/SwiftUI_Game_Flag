d47fae9ce9870b86e04461559da18d65e9932ce4
Kir Proto Q <kir.proto@ya.ru>
2022-02-10 06:27:43 +0500

    Initial Commit

---
+++ Yan_SwiftUI_Game_Flag/Yan_SwiftUI_Game_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_Game_FlagApp.swift
+//  Yan_SwiftUI_Game_Flag
+//
+//  Created by KIR Q on 2022.02.10.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_Game_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
